1a689136670f7d2e069f01ff01d4c6aba3ea904c
davinju226-dot <davin.ju2.26@gmail.com>
2026-05-19 11:27:27 +0700

    Initial commit

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+# ── Terraform settings ─────────────────────────────────────
+# Do NOT add provider blocks here.
+# All providers are configured in main.tf
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
